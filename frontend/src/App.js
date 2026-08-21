9d0c594d806bd5280e2fabd0a21b6eb0899a9874
user <rrovensky@mail.ru>
2026-08-21 12:05:10 +0300

    fix routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-const apiBaseUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const apiBaseUrl = process.env.REACT_APP_API_URL || '';
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
